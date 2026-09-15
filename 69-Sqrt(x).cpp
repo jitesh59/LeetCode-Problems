ec77b7732d051eecf51c4ee6b637854945b9718f
jitesh59 <jiteshnayak70@gmail.com>
2026-09-15 11:01:09 +0530

    [69] [Time Beats: 5.47%] [Memory Beats: 51.12%] - LeetPush

---
+++ 69-Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        // return sqrt(x);
+        if(x == 0 || x==1){
+            return x;
+        }
+        for(long long i=1;i<=x;i++){
+            if(i*i == x){
+                return i;
+            }
+            if(i*i > x){
+                return i-1;
+            }
+        }
+        return 0;
+    }
+};
