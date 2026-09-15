578592a5734415ea53cbf45bc7e864682c7c2df3
jitesh59 <jiteshnayak70@gmail.com>
2026-09-15 09:49:37 +0530

    [231] [Time Beats: 3.31%] [Memory Beats: 12.80%] - LeetPush

--- 231-Power-of-Two.cpp
+++ 231-Power-of-Two.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && (n &(n-1)) ==0;   
+        // return n>0 && (n &(n-1)) ==0;   
+        for (int i=0;i<31;i++){
+            if(pow(2,i) == n){
+                return true;
+            }
+        }
+            return false;
+        
+
     }
 };
