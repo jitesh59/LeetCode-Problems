942b5d889b5eaccb8d21d69f0948f4e351851293
jitesh59 <jiteshnayak70@gmail.com>
2026-09-15 10:00:19 +0530

    [326] [Time Beats: 100.00%] [Memory Beats: 42.93%] - LeetPush

--- 326-Power-of-Three.cpp
+++ 326-Power-of-Three.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+
+        // method 1
         int mp = pow(3,19);
         return n>0 && mp % n == 0;
+
+
+        // // method 2
+        // if(n==1){
+        //     return true;
+        // }
+        // // int ans=1;
+        // for(int i=0;i<19;i++){
+        //     int ans=pow(3,i);
+        //     if(ans  == n){
+        //         return true;
+        //     }
+        //     if(ans >n){
+        //         return false;
+        //     }
+        // }
+        // return false;
     }
 };
