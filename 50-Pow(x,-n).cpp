59031bd1cb13f6852b131de8667f042b61de91f3
jitesh59 <jiteshnayak70@gmail.com>
2026-09-25 18:49:05 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 71.75%] - LeetPush

--- 50-Pow(x,-n).cpp
+++ 50-Pow(x,-n).cpp
@@ -20,8 +20,28 @@ public:
 
 
         // method 2---
-        return pow(x,n);
+        // return pow(x,n);
 
+        // method 3--
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        if(x==1) return 1.0;
+        if(x ==-1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 !=0) return -1.0;
+        long binform = n;
+        if(n<0){
+            x =1/x;
+            binform = -binform;
+        }
+        double ans =1;
+        while (binform > 0 ){
+            if(binform % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            binform /= 2;
+        }
+        return ans;
     
     }
 };
